d74b1e42f5cfabb93c831e4b14b89c4500e812b0
henri983 <henrinyame2001@gmail.com>
2026-09-18 13:38:09 +0000

    TP1: validation opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 16;
+    int b = 3;
+
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %.2f\n", (double)a / b);
+    printf("a %% b = %d\n", a % b);
+    printf("a == b : %d\n", a == b);
+    printf("a > b : %d\n", a > b);
+
+    return 0;
+}
 
 
