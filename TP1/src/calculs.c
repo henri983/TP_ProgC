92e1ac9fc09ebfffa7671eed3c0b1bf52e674dad
henri983 <henrinyame2001@gmail.com>
2026-09-18 13:38:02 +0000

    TP1: validation calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+
+int main(void) {
+    int num1 = 10;
+    int num2 = 5;
+    char op = '+';
+
+    switch (op) {
+        case '+':
+            printf("%d + %d = %d\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%d - %d = %d\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%d * %d = %d\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            printf("%d / %d = %.2f\n", num1, num2, (double)num1 / num2);
+            break;
+        case '%':
+            printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+            break;
+        case '&':
+            printf("%d & %d = %d\n", num1, num2, num1 & num2);
+            break;
+        case '|':
+            printf("%d | %d = %d\n", num1, num2, num1 | num2);
+            break;
+        case '~':
+            printf("~%d = %d\n", num1, ~num1);
+            break;
+        default:
+            printf("Operateur invalide.\n");
+            break;
+    }
+
+    return 0;
+}
 
